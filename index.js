fd0a74327c9fea9c2ac535ef6fd1b4f0f385f373
mdsahil32 <mohammedsahil3211@gmail.com>
2023-07-23 01:02:58 +0600

    set college

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -33,6 +33,13 @@ async function run() {
             const result = await collegeCollection.find().toArray()
             res.send(result)
         })
+        app.get('/colleges/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await collegeCollection.findOne(query)
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
